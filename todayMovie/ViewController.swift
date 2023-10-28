8808fb38e6d9fc40ef70a799125ac10b1f4698eb
김지하 <kimjiha1112@gmail.com>
2023-10-28 21:57:22 +0900

    Initial Commit

---
+++ todayMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todayMovie
+//
+//  Created by Jihaha kim on 2023/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
