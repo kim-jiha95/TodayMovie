604247aaff7d0988729ec46f6d8de4860d3db483
김지하 <kimjiha1112@gmail.com>
2023-11-01 23:52:39 +0900

    add fetchData button - delete storyboard

--- todayMovie/AppDelegate.swift
+++ todayMovie/AppDelegate.swift
@@ -11,11 +11,19 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        if #available(iOS 13.0, *) {
+                    return true
+                }
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+            let viewController = ViewController() 
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+                return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
--- todayMovie/ViewController.swift
+++ todayMovie/ViewController.swift
@@ -12,35 +12,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [Movie] = []
+    
+    let fetchDataButton: UIButton = {
+          let button = UIButton()
+          button.setTitle("Fetch Data", for: .normal)
+          button.backgroundColor = .blue
+          button.setTitleColor(.white, for: .normal)
+        // todo : self warning 고치면 Terminating app due to uncaught exception 'NSInvalidArgumentException
+          button.addTarget(self, action: #selector(fetchDataButtonPressed), for: .touchUpInside)
+          return button
+      }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchData()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(fetchDataButton)
+        fetchDataButton.frame = CGRect(x: 100, y: 400, width: 200, height: 40)
+        
     }
+    
+    @objc func fetchDataButtonPressed() {
+            fetchData()
+        }
+    
     func fetchData() {
         let configuration = URLSessionConfiguration.default
         let session = URLSession(configuration: configuration)
         let tmdbAPIKey = "e3269631c1a855227a37cefab44ad995"
         let urlString = "https://api.themoviedb.org/3/movie/157336?api_key=\(tmdbAPIKey)&append_to_response=videos"
         
-        if let url = URL(string: urlString) {
-            let task = session.dataTask(with: url) { data, response, error in
-                if let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) {
-                    if let data = data {
-                        if let result = String(data: data, encoding: .utf8) {
-                            print(result)
-                        }
-                    }
-                } else {
-                    print("--> response \(response)")
-                }
-            }
-            task.resume()
-        } else {
+        guard let url = URL(string: urlString) else {
             print("Invalid URL")
+            return
         }
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                print("--> response \(String(describing: response))")
+                return
+            }
+            
+            guard let data = data, let result = String(data: data, encoding: .utf8) else {
+                return
+            }
+            
+            print("Movie result:", result)
+        }
+        
+        task.resume()
     }
-    
+
     
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return movies.count
